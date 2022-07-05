d853dcf26b755cdf88fdb8d56b761c78b443fda6
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2022-07-05 20:39:47 +0530

    Add files via upload

---
+++ 09_python_modules/lab.py
@@ -0,0 +1,19 @@
+# Write a program that prints the time taken to produce all the prime numbers
+# between a given range
+
+# input
+
+
+# process
+
+
+# output
+
+
+''' 
+  1 - 1000 -> time
+  1000 - 2000 -> time
+  2000 - 3000 -> time
+  ....
+  9000 - 10000 -> time
+'''
